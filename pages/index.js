f287d88e18d98693083b862792de2ad4906fabd6
Jonathan Lauxen Romano <jjonathanromano@gmail.com>
2022-04-07 22:53:17 -0300

    Melhorando o estilo da pagina, pedi ajuda da Aline kkkk

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import ExperienceCard from '../src/components/experiencia_card'
+import Image from 'next/image'
+import myPic from '../src/images/eu.jpeg'
+import instagram from '../src/images/instaLogo.png'
+import github from '../src/images/githubLogo.png'
+import linkedin from '../src/images/linkedinLogo.png'
+import whatsapp from '../src/images/whatsappLogo.png'
 
 export async function getStaticProps(context){
     const { MongoClient } = require('mongodb')
@@ -25,7 +31,7 @@ export async function getStaticProps(context){
 
     return {
         props: {
-            result: result.data
+            result: result.data,
         }
     }
 }
@@ -33,6 +39,7 @@ export async function getStaticProps(context){
 export default function Home(props) {
 
     const experienceArray = props.result.experiences
+    const description = props.result.description
 
     return (
         <div className={styles.container}>
@@ -42,13 +49,50 @@ export default function Home(props) {
                 <meta charSet="utf-8"/>
             </Head>
 
-
             <main>
                 <div className={styles.title}>
                     <h1>Jonathan Lauxen Romano</h1>
                 </div>
+                
+                <div className={styles.imageBox}>
+                    <div className={styles.myPic}>
+                        <Image src={myPic}/>
+                    </div>
+                </div>
+                
+                <p>
+                    {description}
+                </p>
 
                 {experienceArray.map(item => <ExperienceCard experienceObject={item} />)}
+
+                <div className={styles.socialMediaBox}>
+                    
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://instagram.com/jonathan_lauxen_?igshid=YmMyMTA2M2Y=" target="_blank">
+                            <Image src={instagram} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://www.linkedin.com/in/jonathan-lauxen-romano-626718193/" target="_blank">
+                            <Image src={linkedin} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://github.com/JonathanRomano" target="_blank">
+                            <Image src={github} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://api.whatsapp.com/send?phone=5541992388985&text=Ol%c3%a1%2c+visitei+o+seu+site.+Podemos+conversar%3f" target="_blank">
+                            <Image src={whatsapp} />
+                        </a>
+                    </div>
+
+                </div>
 
             </main>
 
