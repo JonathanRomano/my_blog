064bc067ae281cd11e14ed0ce9ca810dff4d0056
Jonathan Lauxen Romano <jjonathanromano@gmail.com>
2022-04-14 14:42:50 -0300

    teste

---
+++ pages/blog/index.js
@@ -0,0 +1,109 @@
+import styles from "../../styles/Blog.module.css"
+import Head from "next/head"
+import PostObject from "../../src/components/PostObject"
+import Image from "next/image"
+import * as instagram from '../../src/images/instaLogo.png'
+import * as github from '../../src/images/githubLogo.png'
+import * as linkedin from '../../src/images/linkedinLogo.png'
+import * as whatsapp from '../../src/images/whatsappLogo.png'
+import * as euzinho from '../../src/images/eu.jpeg'
+import Link from "next/link"
+import { GetStaticProps } from "next"
+
+export const getStaticProps = async () => {
+    const { MongoClient } = require('mongodb')
+
+    const mongo_user = process.env.MONGO_DB_USER
+    const mongo_password = process.env.MONGO_DB_PASSWORD
+    
+        const mongo_uri = "mongodb+srv://"+mongo_user+":"+mongo_password+"@cluster0.szi09.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+    const client = new MongoClient(mongo_uri, {useNewUrlParser: true})
+
+    async function findOneByType(client){
+        await client.connect()
+
+        const posts = client.db("Cluster0").collection("blog_collection")
+        
+        const query = {'type':'normal'}
+        const options = {
+            projection:{
+                _id: 0,
+                text: 0
+            }
+        }
+
+        const cursor = posts.find(query,options).toArray()
+
+        return cursor
+    }
+
+    const result = await findOneByType(client)
+
+    return {
+        props: {
+            postsArray: result,
+        },
+        revalidate: 120
+    }
+}
+
+export default function (props) {
+    const lista_de_posts = props.postsArray
+
+    return(
+        <div className={styles.container}>
+            <Head>
+                <title>Jonathan Lauxen Romano</title>
+                <meta name="description" content="Programador iniciante em busca de novas aventuras." />
+                <meta charSet="utf-8"/>
+            </Head>
+
+            <main>
+                <div className={styles.headerBox}>
+
+                    <Link href='/'>
+                        <a className={styles.backButton}>
+                            &#10158;
+                        </a>                    
+                    </Link>
+
+                    <h1 className={styles.title}>
+                        Blog
+                    </h1>
+                </div>
+
+                <div className={styles.postsBox}>
+                    
+                    {lista_de_posts.map(item => <PostObject postObject={item} postImage={require('../../src/images/' + item.slug + '.png')}/>)}
+                </div>
+
+                <footer className={styles.socialMediaBox}>
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://github.com/JonathanRomano" target="_blank">
+                            <Image src={github} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://www.linkedin.com/in/jonathan-lauxen-romano-626718193/" target="_blank">
+                            <Image src={linkedin} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://instagram.com/jonathan_lauxen_?igshid=YmMyMTA2M2Y=" target="_blank">
+                            <Image src={instagram} />
+                        </a>
+                    </div>
+
+                    <div className={styles.socialMediaIcon}>
+                        <a href="https://api.whatsapp.com/send?phone=5541992388985&text=Ol%c3%a1%2c+visitei+o+seu+site.+Podemos+conversar%3f" target="_blank">
+                            <Image src={whatsapp} />
+                        </a>
+                    </div>
+                </footer>
+            </main>
+        </div>
+    )
+}
