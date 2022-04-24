69c90f53bb90b6cb6def425360dde62fcc4f72d8
Jonathan Lauxen Romano <jonathan@Emily-3.0>
2022-04-24 20:13:55 -0300

    Como todo erro que me tira do serio, foi extremamente imbecil! KKKKKKK

--- src/components/ExperienciaCard/index.js
+++ src/components/ExperienciaCard/index.js
@@ -1,5 +1,5 @@
 import React, {useState} from "react"
-import styles from "../oldPage.module.css"
+import styles from "../../../styles/oldPage.module.css"
 
 const ExperienceCard = ({experienceObject}) => {
 
